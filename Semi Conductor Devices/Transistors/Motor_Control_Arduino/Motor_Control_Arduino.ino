0b6fd989d9208495622fef161d09ac97a98514ae
AR123456 <30957071+AR123456@users.noreply.github.com>
2021-05-29 09:10:12 -0600

    control motor with arduion

---
+++ Semi Conductor Devices/Transistors/Motor_Control_Arduino/Motor_Control_Arduino.ino
@@ -0,0 +1,30 @@
+int motorPinR = 0; // control pin for RIGHT side
+int motorPinL = 1; // control pin for LEFT side
+
+void setup() {
+  // set the digital pins as outputs
+  pinMode(motorPinR, OUTPUT);
+  pinMode(motorPinL, OUTPUT);
+}
+
+void loop() {
+  // Turn both sides off & wait 1 second
+  digitalWrite(motorPinR, LOW);
+  digitalWrite(motorPinL, LOW);
+  delay(1000);
+
+  // Turn only right side on & wait 1 second
+  digitalWrite(motorPinR, HIGH);
+  digitalWrite(motorPinL, LOW);
+  delay(1000);
+
+  // Turn both sides on & wait 1 second
+  digitalWrite(motorPinR, HIGH);
+  digitalWrite(motorPinL, HIGH);
+  delay(1000);
+
+  // Turn only left side on & wait 1 second
+  digitalWrite(motorPinR, LOW);
+  digitalWrite(motorPinL, HIGH);
+  delay(1000);
+}
