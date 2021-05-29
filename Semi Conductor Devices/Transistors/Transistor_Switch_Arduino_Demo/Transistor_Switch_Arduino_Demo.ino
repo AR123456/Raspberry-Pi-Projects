3c7329bec481bf71e2fc2b6cc33a374346f24d8b
AR123456 <30957071+AR123456@users.noreply.github.com>
2021-05-29 09:09:02 -0600

    arduino switch

---
+++ Semi Conductor Devices/Transistors/Transistor_Switch_Arduino_Demo/Transistor_Switch_Arduino_Demo.ino
@@ -0,0 +1,12 @@
+int motorPin = 0;
+
+void setup() {
+  pinMode(motorPin, OUTPUT); // sets the digital pin as output
+}
+
+void loop() {
+  digitalWrite(motorPin, HIGH);   // sets the LED on
+  delay(1000);                  // waits for a second
+  digitalWrite(motorPin, LOW);    // sets the LED off
+  delay(1000);                  // waits for a second
+}
